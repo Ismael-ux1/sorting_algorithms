3d1f4ec4b0c833d599f0a5c81a4c8054850ac0df
ismael-ux-1 <ismaelabdulahi@gmail.com>
2023-07-27 13:58:54 -0700

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -23,6 +23,6 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void lomuto_sort(int *array, int lo, int hi, size_t size);
+void quicksort_a(int *array, size_t low, size_t high);
 
 #endif
